783aa0bebe57d17b9f464ea322bc6240a41ba3fe
Tyler Yeats <tyeats@uw.edu>
2017-02-04 23:36:59 -0800

    Updated git ignore

--- CampfireNet/IdentityService/IdentityManager.cs
+++ CampfireNet/IdentityService/IdentityManager.cs
@@ -6,35 +6,36 @@ using System.Threading.Tasks;
 
 namespace IdentityService
 {
-    class IdentityManager
-    {
-        private Dictionary<string, Identity> identityTable;
-        // may leave space for broadcast scheme
-        public IdentityManager()
-        {
-            identityTable = new Dictionary<string, Identity>();
-        }
+	class IdentityManager
+	{
+		private Dictionary<string, Identity> identityTable;
+		// may leave space for broadcast scheme
+		public IdentityManager()
+		{
+			identityTable = new Dictionary<string, Identity>();
+		}
 
-        public Identity LookupIdentity(string user)
-        {
-            if(identityTable.TryGetValue(user, out Identity identity))
-            {
-                return identity;
-            }
-            return null;
-        }
+		public Identity LookupIdentity(string user)
+		{
+			Identity identity;
+			if (identityTable.TryGetValue(user, out identity))
+			{
+				return identity;
+			}
+			return null;
+		}
 
-        public void AddIdentity(string user, Identity identity)
-        {
-            identityTable.Add(user, identity);
-        }
+		public void AddIdentity(string user, Identity identity)
+		{
+			identityTable.Add(user, identity);
+		}
 
-        public void UpdateIdentity(List<string> users, List<Identity> identities)
-        {
-            for(int i = 0; i < users.Count; i++)
-            {
-                identityTable.Add(users.ElementAt(i), identities.ElementAt(i));
-            }
-        }
-    }
+		public void UpdateIdentity(List<string> users, List<Identity> identities)
+		{
+			for (int i = 0; i < users.Count; i++)
+			{
+				identityTable.Add(users.ElementAt(i), identities.ElementAt(i));
+			}
+		}
+	}
 }
