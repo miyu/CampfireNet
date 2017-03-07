a6807c0ef24c6ab65db462b5bf46f1e24e279322
Yufang Sun <mandary@uw.edu>
2017-03-07 15:22:19 -0800

    Updated setting with shared preferences

--- CampfireChat/CampfireChat/SettingsActivity.cs
+++ CampfireChat/CampfireChat/SettingsActivity.cs
@@ -3,6 +3,10 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Widget;
+using CampfireNet;
+using CampfireNet.Identities;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace CampfireChat
 {
@@ -10,19 +14,42 @@ namespace CampfireChat
 	public class SettingsActivity : Activity
 	{
 		const int PICKFILE_RESULT_CODE = 1;
+        private ISharedPreferences prefs;
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Settings);
-			var toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+            
+            var toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
 			SetActionBar(toolbar);
 			ActionBar.SetDisplayHomeAsUpEnabled(true);
 
-			var generateRoot = FindViewById<LinearLayout>(Resource.Id.BecomeRoot);
+            prefs = Application.Context.GetSharedPreferences("CampfireChat", FileCreationMode.Private);
+
+            var generateRoot = FindViewById<LinearLayout>(Resource.Id.BecomeRoot);
 			generateRoot.Click += (sender, e) =>
 			{
-				Toast.MakeText(this, "Action selected: generate root", ToastLength.Short).Show();
-			};
+                var userName = prefs.GetString("Name", null);
+                var rsa = Helper.InitRSA(prefs);
+                Identity identity = new Identity(new IdentityManager(), rsa, userName);
+                var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                path = Path.Combine(path, $"trust_chain_{IdentityManager.GetIdentityString(identity.PublicIdentityHash)}.tc");
+
+                if (!File.Exists(path) && identity.TrustChain == null)
+                {
+                    identity.GenerateRootChain();
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.Write(TrustChainUtil.SerializeTrustChain(identity.TrustChain));
+                    }
+                }
+                else
+                {
+                    Toast.MakeText(ApplicationContext, "Trust chain already exists.", ToastLength.Short).Show();
+                }
+            };
 
 			var loadChain = FindViewById<LinearLayout>(Resource.Id.LoadChain);
 			loadChain.Click += (sender, e) =>
@@ -38,8 +65,8 @@ namespace CampfireChat
 			var inviteFriend = FindViewById<LinearLayout>(Resource.Id.Invite);
 			inviteFriend.Click += (sender, e) =>
 			{
-				Toast.MakeText(this, "Action selected: invite friend", ToastLength.Short).Show();
-			};
+
+            };
 		}
 	}
 }
