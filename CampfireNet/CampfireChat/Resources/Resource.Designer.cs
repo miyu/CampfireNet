f4e7298265d592e6f3086c59c3def0618a0ed34a
Tyler Yeats <tyeats@uw.edu>
2017-03-08 08:18:46 -0800

    Implemented settings features

--- CampfireNet/CampfireChat/Resources/Resource.Designer.cs
+++ CampfireNet/CampfireChat/Resources/Resource.Designer.cs
@@ -1704,70 +1704,73 @@ namespace CampfireChat
 			public const int ic_add_white_48dp = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int ic_open_in_new_white_48dp = 2130837588;
+			public const int ic_delete_white_48dp = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int ic_person_add_white_48dp = 2130837589;
+			public const int ic_open_in_new_white_48dp = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int ic_person_white_48dp = 2130837590;
+			public const int ic_person_add_white_48dp = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int ic_send_white_48dp = 2130837591;
+			public const int ic_person_white_48dp = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int ic_settings_input_antenna_white_48dp = 2130837592;
+			public const int ic_send_white_48dp = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int ic_settings_white_48dp = 2130837593;
+			public const int ic_settings_input_antenna_white_48dp = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int ic_wifi_tethering_white_48dp = 2130837594;
+			public const int ic_settings_white_48dp = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int Icon = 2130837595;
+			public const int ic_wifi_tethering_white_48dp = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int notification_action_background = 2130837596;
+			public const int Icon = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int notification_bg = 2130837597;
+			public const int notification_action_background = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_bg_low = 2130837598;
+			public const int notification_bg = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg_low_normal = 2130837599;
+			public const int notification_bg_low = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low_pressed = 2130837600;
+			public const int notification_bg_low_normal = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_normal = 2130837601;
+			public const int notification_bg_low_pressed = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_normal_pressed = 2130837602;
+			public const int notification_bg_normal = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_icon_background = 2130837603;
-			
-			// aapt resource value: 0x7f020068
-			public const int notification_template_icon_bg = 2130837608;
-			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_low_bg = 2130837609;
+			public const int notification_bg_normal_pressed = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_tile_bg = 2130837604;
+			public const int notification_icon_background = 2130837604;
+			
+			// aapt resource value: 0x7f020069
+			public const int notification_template_icon_bg = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int notification_template_icon_low_bg = 2130837610;
 			
 			// aapt resource value: 0x7f020065
-			public const int notify_panel_notification_icon_bg = 2130837605;
+			public const int notification_tile_bg = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int Splash = 2130837606;
+			public const int notify_panel_notification_icon_bg = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int splash_screen = 2130837607;
+			public const int Splash = 2130837607;
+			
+			// aapt resource value: 0x7f020068
+			public const int splash_screen = 2130837608;
 			
 			static Drawable()
 			{
@@ -1782,14 +1785,17 @@ namespace CampfireChat
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f090081
-			public const int AddChatRoom = 2131296385;
+			// aapt resource value: 0x7f090083
+			public const int AddChatRoom = 2131296387;
 			
 			// aapt resource value: 0x7f09007d
 			public const int BecomeRoot = 2131296381;
 			
 			// aapt resource value: 0x7f090062
 			public const int ChatList = 2131296354;
+			
+			// aapt resource value: 0x7f090081
+			public const int ClearAll = 2131296385;
 			
 			// aapt resource value: 0x7f090065
 			public const int ContactList = 2131296357;
@@ -1824,17 +1830,20 @@ namespace CampfireChat
 			// aapt resource value: 0x7f09005e
 			public const int Preview = 2131296350;
 			
-			// aapt resource value: 0x7f090082
-			public const int SearchFriend = 2131296386;
+			// aapt resource value: 0x7f090084
+			public const int SearchFriend = 2131296388;
 			
 			// aapt resource value: 0x7f090059
 			public const int SendBar = 2131296345;
 			
+			// aapt resource value: 0x7f090080
+			public const int SendId = 2131296384;
+			
 			// aapt resource value: 0x7f09005b
 			public const int SendMessage = 2131296347;
 			
-			// aapt resource value: 0x7f090080
-			public const int Settings = 2131296384;
+			// aapt resource value: 0x7f090082
+			public const int Settings = 2131296386;
 			
 			// aapt resource value: 0x7f090061
 			public const int Tag = 2131296353;
--- CampfireNet/CampfireChat/SettingsActivity.cs
+++ CampfireNet/CampfireChat/SettingsActivity.cs
@@ -3,60 +3,184 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Widget;
-using AndroidTest.Droid;
-using CampfireNet;
 using CampfireNet.Identities;
 using System.IO;
-using System.Security.Cryptography;
+using Android.Views;
+using System;
 
 namespace CampfireChat {
    [Activity(Label = "Settings", ParentActivity = typeof(MainActivity))]
    public class SettingsActivity : Activity {
-      const int PICKFILE_RESULT_CODE = 1;
+      public const int CHOOSE_TRUST_CHAIN_FILE = 0;
+      public const int CHOOSE_IDENTITY_FILE = 1;
+
+      public const string TRUST_CHAIN_FORMAT_STRING = "trust_chain_{0}.tc";
+      public const string IDENTITY_FORMAT_STRING = "identity_{0}.id";
+
+      private Toolbar toolbar;
+      private LinearLayout becomeRoot;
+      private LinearLayout loadTrustChain;
+      private LinearLayout inviteFriend;
+      private LinearLayout sendId;
+      private LinearLayout clearAllKnown;
+
       private ISharedPreferences prefs;
+      private Identity identity;
+      private string privatePath;
+
       protected override void OnCreate(Bundle savedInstanceState) {
          base.OnCreate(savedInstanceState);
          SetContentView(Resource.Layout.Settings);
 
-         var toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+         toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
          SetActionBar(toolbar);
          ActionBar.SetDisplayHomeAsUpEnabled(true);
 
-         var prefs = Application.GetSharedPreferences("CampfireChat", FileCreationMode.Private);
+         becomeRoot = FindViewById<LinearLayout>(Resource.Id.BecomeRoot);
+         loadTrustChain = FindViewById<LinearLayout>(Resource.Id.LoadChain);
+         inviteFriend = FindViewById<LinearLayout>(Resource.Id.Invite);
+         sendId = FindViewById<LinearLayout>(Resource.Id.SendId);
+         clearAllKnown = FindViewById<LinearLayout>(Resource.Id.ClearAll);
 
-         var identity = Globals.CampfireNetClient.Identity;
-         var generateRoot = FindViewById<LinearLayout>(Resource.Id.BecomeRoot);
 
-         generateRoot.Click += (sender, e) => {
-            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            path = Path.Combine(path, $"trust_chain_{IdentityManager.GetIdentityString(identity.PublicIdentityHash)}.tc");
-            if (!File.Exists(path) && identity.TrustChain == null) {
+         prefs = Application.GetSharedPreferences("CampfireChat", FileCreationMode.Private);
+
+         identity = Globals.CampfireNetClient.Identity;
+         privatePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+         becomeRoot.Click += (sender, e) => {
+            var fileName = string.Format(TRUST_CHAIN_FORMAT_STRING,
+                                         IdentityManager.GetIdentityString(identity.PublicIdentityHash));
+            var fullPath = Path.Combine(privatePath, fileName);
+
+            if (!File.Exists(fullPath) && identity.TrustChain == null) {
                identity.GenerateRootChain();
-               byte[] trustChain = TrustChainUtil.SerializeTrustChain(identity.TrustChain);
-               Helper.UpdateTrustChain(prefs, trustChain);
-               using (var stream = new FileStream(path, FileMode.Create))
+
+               byte[] trustChainBytes = TrustChainUtil.SerializeTrustChain(identity.TrustChain);
+
+               using (var stream = new FileStream(fullPath, FileMode.Create))
                using (var writer = new BinaryWriter(stream)) {
-                  writer.Write(trustChain);
+                  writer.Write(trustChainBytes);
                }
+               Toast.MakeText(ApplicationContext, "Root node successfully created", ToastLength.Short).Show();
             } else {
-               Toast.MakeText(ApplicationContext, "Trust chain already exists.", ToastLength.Short).Show();
+               Toast.MakeText(ApplicationContext, "Trust chain already exists", ToastLength.Short).Show();
             }
          };
 
-         var loadChain = FindViewById<LinearLayout>(Resource.Id.LoadChain);
-         loadChain.Click += (sender, e) => {
-            Toast.MakeText(this, "Action selected: load chain", ToastLength.Short).Show();
+         loadTrustChain.Click += (sender, e) => {
             Intent chooseFile = new Intent(Intent.ActionGetContent);
             chooseFile.AddCategory(Intent.CategoryOpenable);
             chooseFile.SetType("text/plain");
-            chooseFile = Intent.CreateChooser(chooseFile, "Choose a file");
-            StartActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+            chooseFile = Intent.CreateChooser(chooseFile, "Choose a trust chain file to load");
+
+            Toast.MakeText(ApplicationContext, "Choose a trust chain file to load (*.tc)", ToastLength.Short).Show();
+
+            StartActivityForResult(chooseFile, CHOOSE_TRUST_CHAIN_FILE);
          };
 
-         var inviteFriend = FindViewById<LinearLayout>(Resource.Id.Invite);
          inviteFriend.Click += (sender, e) => {
+            Intent chooseFile = new Intent(Intent.ActionGetContent);
+            chooseFile.AddCategory(Intent.CategoryOpenable);
+            chooseFile.SetType("text/plain");
+            chooseFile = Intent.CreateChooser(chooseFile, "Choose an identity to load");
 
+            Toast.MakeText(ApplicationContext, "Choose an identity to load (*.id)", ToastLength.Short).Show();
+
+            StartActivityForResult(chooseFile, CHOOSE_IDENTITY_FILE);
          };
+
+         sendId.Click += (sender, e) => {
+            var idBytes = identity.PublicIdentity;
+
+            var idFileName = string.Format(IDENTITY_FORMAT_STRING, IdentityManager.GetIdentityString(idBytes));
+            var idFullPath = Path.Combine(privatePath, idFileName);
+            File.WriteAllBytes(idFullPath, idBytes);
+
+            startEmail("Identification", idFullPath);
+         };
+
+         clearAllKnown.Click += (sender, e) => {
+            foreach (var file in Directory.GetFiles(privatePath)) {
+               if (file.EndsWith(".tc", StringComparison.Ordinal) || file.EndsWith(".id", StringComparison.Ordinal)) {
+                  File.Delete(file);
+                  Console.WriteLine($"deleting {file}");
+               }
+            }
+         };
+      }
+
+      protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
+         base.OnActivityResult(requestCode, resultCode, data);
+
+         if (resultCode == Result.Ok && data != null) {
+            if (requestCode == CHOOSE_TRUST_CHAIN_FILE) {
+               var fullPath = data.Data.Path;
+
+               byte[] trustChainBytes = File.ReadAllBytes(fullPath);
+
+               if (trustChainBytes.Length % TrustChainNode.NODE_BLOCK_SIZE != 0) {
+                  Toast.MakeText(this, "Trust chain is invalid length", ToastLength.Short).Show();
+               } else if (!identity.ValidateAndAdd(trustChainBytes)) {
+                  Toast.MakeText(this, "Could not validate the trust chain", ToastLength.Short).Show();
+               } else {
+                  Toast.MakeText(this, "Could not validate the trust chain", ToastLength.Short).Show();
+               }
+            } else if (requestCode == CHOOSE_IDENTITY_FILE) {
+               var fullPath = data.Data.Path;
+               Console.WriteLine($"got path {fullPath}");
+
+               byte[] newIdentity = File.ReadAllBytes(fullPath);
+
+               if (newIdentity.Length != CryptoUtil.ASYM_KEY_SIZE_BYTES) {
+                  Toast.MakeText(this, "Identity is invalid length", ToastLength.Short).Show();
+               } else {
+                  // TODO add changeable permissions
+                  var newChainBytes = identity.GenerateNewChain(newIdentity, identity.PermissionsHeld,
+                                                                identity.PermissionsGrantable, "Child");
+
+                  var trustChainFileName = string.Format(TRUST_CHAIN_FORMAT_STRING,
+                                               IdentityManager.GetIdentityString(newIdentity));
+                  var trustChainFullPath = Path.Combine(privatePath, trustChainFileName);
+                  File.WriteAllBytes(trustChainFullPath, newChainBytes);
+
+                  startEmail("Trust chain", trustChainFullPath);
+               }
+            }
+         }
+      }
+
+      private void startEmail(string subject, string filePath) {
+         var index = filePath.LastIndexOf("/", StringComparison.Ordinal);
+         var fileName = filePath.Substring(index + 1, filePath.Length - index - 1);
+
+         var tmpFolder = Android.OS.Environment.ExternalStorageDirectory.ToString();
+         var tmpPath = Path.Combine(tmpFolder, fileName);
+         File.Copy(filePath, tmpPath, true);
+
+         var file = new Java.IO.File(tmpPath);
+         file.SetReadable(true, false);
+         var uri = Android.Net.Uri.FromFile(file);
+
+         Intent email = new Intent(Intent.ActionSend);
+         email.SetType("message/rfc822");
+         email.PutExtra(Intent.ExtraSubject, subject);
+         email.PutExtra(Intent.ExtraStream, uri);
+         email.AddFlags(ActivityFlags.GrantReadUriPermission);
+
+         try {
+            StartActivityForResult(Intent.CreateChooser(email, "Send mail with: "), 0);
+         } catch (ActivityNotFoundException) {
+            Toast.MakeText(ApplicationContext, "There are no email clients installed.", ToastLength.Short).Show();
+         }
+      }
+
+      public override bool OnOptionsItemSelected(IMenuItem item) {
+         if (item.ItemId == Android.Resource.Id.Home) {
+            Finish();
+         }
+
+         return base.OnOptionsItemSelected(item);
       }
    }
 }
