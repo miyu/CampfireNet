e6e132c501dd41d96ed39b750d1334216013935f
Yufang Sun <mandary@uw.edu>
2017-03-08 03:44:14 -0800

    added person activity

--- CampfireNet/CampfireChat/SettingsActivity.cs
+++ CampfireNet/CampfireChat/SettingsActivity.cs
@@ -1,11 +1,13 @@
 
 using Android.App;
 using Android.Content;
+using Android.Net;
 using Android.OS;
 using Android.Widget;
 using AndroidTest.Droid;
 using CampfireNet;
 using CampfireNet.Identities;
+using System;
 using System.IO;
 using System.Security.Cryptography;
 
@@ -27,17 +29,17 @@ namespace CampfireChat {
          var identity = Globals.CampfireNetClient.Identity;
          var generateRoot = FindViewById<LinearLayout>(Resource.Id.BecomeRoot);
 
+         var filename = $"trust_chain_{IdentityManager.GetIdentityString(identity.PublicIdentityHash)}.tc";
+
          generateRoot.Click += (sender, e) => {
-            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            path = Path.Combine(path, $"trust_chain_{IdentityManager.GetIdentityString(identity.PublicIdentityHash)}.tc");
+            var dir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(dir, filename);
             if (!File.Exists(path) && identity.TrustChain == null) {
                identity.GenerateRootChain();
                byte[] trustChain = TrustChainUtil.SerializeTrustChain(identity.TrustChain);
                Helper.UpdateTrustChain(prefs, trustChain);
-               using (var stream = new FileStream(path, FileMode.Create))
-               using (var writer = new BinaryWriter(stream)) {
-                  writer.Write(trustChain);
-               }
+               Helper.WriteToFile(path, trustChain);
+               Toast.MakeText(ApplicationContext, "Root chain generated.", ToastLength.Short).Show();
             } else {
                Toast.MakeText(ApplicationContext, "Trust chain already exists.", ToastLength.Short).Show();
             }
@@ -45,23 +47,30 @@ namespace CampfireChat {
 
          var loadChain = FindViewById<LinearLayout>(Resource.Id.LoadChain);
          loadChain.Click += (sender, e) => {
-            Toast.MakeText(this, "Action selected: load chain", ToastLength.Short).Show();
+
             Intent chooseFile = new Intent(Intent.ActionGetContent);
             chooseFile.AddCategory(Intent.CategoryOpenable);
+            chooseFile.SetType("file/*");
             chooseFile = Intent.CreateChooser(chooseFile, "Choose a file");
+            
             StartActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
-            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            path = Path.Combine(path, $"trust_chain_{IdentityManager.GetIdentityString(identity.PublicIdentityHash)}.tc");
 
-            if (!File.Exists(path)) {
-               Toast.MakeText(ApplicationContext, "Cannot find own trust chain file in Download folder.", ToastLength.Short).Show();
+            var uri = (Android.Net.Uri)Intent.GetParcelableExtra("ReceivedChain");
+            var path = uri.Path;
+
+            if (!path.Contains(filename)) {
+               Toast.MakeText(ApplicationContext, "Trust chain file does not match", ToastLength.Short).Show();
                return;
             }
 
-            byte[] data = File.ReadAllBytes(path);
+            byte[] trustChain = File.ReadAllBytes(path);
 
             try {
-               identity.AddTrustChain(data);
+               identity.AddTrustChain(trustChain);
+               var dir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+               var newpath = Path.Combine(dir, filename);
+               Helper.UpdateTrustChain(prefs, trustChain);
+               Helper.WriteToFile(newpath, trustChain);
                Toast.MakeText(ApplicationContext, "Successfully loaded trust chain.", ToastLength.Short).Show();
             } catch (BadTrustChainException) {
                Toast.MakeText(ApplicationContext, "Invalid trust chain found.", ToastLength.Short).Show();
@@ -73,6 +82,14 @@ namespace CampfireChat {
             Intent chooseFile = new Intent(Intent.ActionGetContent);
 
          };
+      }
+
+      protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
+         base.OnActivityResult(requestCode, resultCode, data);
+         if (resultCode == Result.Ok && data != null) {
+            Android.Net.Uri uri = data.Data;
+            data.PutExtra("ReceivedChain", uri);
+         }
       }
    }
 }
